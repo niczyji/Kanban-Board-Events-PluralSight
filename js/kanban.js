38f761b33f2921e3cccb9649071536bcd2cabca8
niczyji <niczyji@gmail.com>
2019-08-22 15:52:47 +0200

    first commit

---
+++ js/kanban.js
@@ -0,0 +1,26 @@
+let order = 1;
+let adding = false;
+
+const error = document.querySelector(".error");
+const message = "Please add a description.";
+
+const add_btn = document.querySelector(".add");
+add_btn.addEventListener("click", () => {
+  const target = document.querySelector("#requested");
+  if (adding == false) {
+    adding = true;
+    target.appendChild(create_item());
+  } else {
+    error.innerHTML = message;
+  }
+});
+
+const create_item = () => {
+  let item = document.createElement("div");
+  item.classList.add("item");
+  item.id = "item-" + order;
+  item.setAttribute("order");
+  item.draggable = true;
+};
+
+document.querySelectorAll(".drop").forEach(element => {});
